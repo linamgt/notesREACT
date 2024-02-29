c75b65c0b72b7bf4ed1d303adccc6648eea875a1
linamgt <lina.magnet64@gmail.com>
2024-02-29 13:24:56 +0100

    installation

--- index.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-// Effacez le contenu HTML existant
-document.body.innerHTML = '<div id="app"></div>';
-
-// Affichez plutôt votre composant React
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Bonjour tout le monde</h1>);
