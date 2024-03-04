617626b9a9bce9702ec2185f4efe2ee5f35ab163
linamgt <lina.magnet64@gmail.com>
2024-03-04 14:21:28 +0100

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,64 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import ArrayLib from './lib/array-lib'
+
 import Counter from './components/Counter'
 import Filters from './components/Filters'
 import NoteList from './components/NoteList'
-import AddNotForm from './components/AddNoteForm'
+import AddNoteForm from './components/AddNoteForm'
 
 function App() {
 
-  const [notes, setNotes] = useState([
+  const pureNotes = [
     { id: 11, text: "première note" },
     { id: 12, text: "deuxième note" },
     { id: 33, text: "troisième note" }
-  ]);
+  ]
+
+  const [notesRAW, notesRAWSetter] = useState([...pureNotes]);
+
+  const [notes, setNotes] = useState([...notesRAW]);
+
+
+  const [filters, filtersSetter] = useState(
+    {keyword: ''}
+  );
 
   function onRemoveBtnHandler(noteToDelete) {
-    // on garde tous les notes qui ne sont pas celle à supprimer
-    const newNotes = notes.filter(note => note.id !== noteToDelete.id);
-    // const newNotes = notes.filter(note => note !== noteToDelete);
-    setNotes(newNotes);
+    const noteRawNewValues = ArrayLib.remove(notesRAW, noteToDelete);
+    notesRAWSetter(noteRawNewValues);
+    updateFiltered(noteRawNewValues);
+  }
+
+  function onNoteAddedHandler(newNote) {
+    const noteRawNewValues = [...notesRAW, newNote];
+    notesRAWSetter(noteRawNewValues);
+    updateFiltered(noteRawNewValues)
+  }
+
+  function updateFiltered(notes) {
+    setNotes([...notes]);
+  }
+
+  function onFilterChangedHandler(keyword) {
+    console.log('filters: ', keyword)
+    filtersSetter({
+      keyword: keyword
+    });
+    if (keyword.length > 0) setNotes(notesRAW.filter(n => n.text.includes(keyword)));
+    else setNotes(notesRAW);
   }
 
   return (
     <>
       <h1>Application Notes</h1>
-      <Counter />
-      <AddNotForm />
-      <Filters />
-      <NoteList notes={notes}  onRemoveBtn={onRemoveBtnHandler} />
+      <Counter notes={notes} />
+      <AddNoteForm onNoteAdded={onNoteAddedHandler} />
+      <Filters filters={filters} onFilterChanged={onFilterChangedHandler} />
+      <NoteList notes={notes} onRemoveBtn={onRemoveBtnHandler} />
     </>
   )
-
-  // const onListItemClickHandler = (e) => {
-  //   console.log(e);
-  // }
-
-  // return (
-  //   <Fragment>
-  //     <NoteList notes={notes} onListItemClick={onListItemClickHandler}></NoteList>
-  //   </Fragment>
-  // )
 }
 
 export default App
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,3 +1,18 @@
-export default function Filters () {
-  return <input />;
+import { useState } from "react";
+
+export default function Filters({filters, onFilterChanged}) {
+
+  const onKeyWordChanged = e => {
+    const keyword = e.target.value;
+    onFilterChanged(keyword);
+  };
+
+  return (
+    <form>
+      <fieldset>
+        <legend>Recherche une note</legend>
+      <input name="keyword" type="search" placeholder="rechercher par mot" value={filters.keyword} onChange={onKeyWordChanged} />
+      </fieldset>
+    </form>
+  );
 }
