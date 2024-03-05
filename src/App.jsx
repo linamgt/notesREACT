f456b9ece77fe1c37c60363e82d2c775fc4116f4
linamgt <lina.magnet64@gmail.com>
2024-03-05 14:01:46 +0100

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { Fragment, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 import ArrayLib from './lib/array-lib'
 
+// Gestionnaire d'entités
+import { NoteManager } from './api/note-manager'
+
+// Composants
 import Counter from './components/Counter'
 import Filters from './components/Filters'
 import NoteList from './components/NoteList'
@@ -12,55 +14,64 @@ import AddNoteForm from './components/AddNoteForm'
 
 function App() {
 
-  // loadNotes();
+  // Déclaration des états du composant.
+  const [notesRAW, setNotesRAW] = useState([]);
+  const [notes, setNotes] = useState([...notesRAW]);
+  const [filters, filtersSetter] = useState({ keyword: '' }); // ajouter ici d'autre propriétés pour filter d'autre façons.
 
+  // Charger les données dès juste après la création du composant.
+  // Comme on fait un appel à un setState pas d'autre chose que de passer par un hook userEffect.
+  // Si on faisait un setState dans App() on aurait => setState() => App() => setState (infinite loop!)
+  // doc: https://fr.react.dev/reference/react/useEffect
   useEffect(() => {
-    fetch('http://localhost:3000/notes/')
-    .then(response => response.json())
-    .then(data => {
-      notesRAWSetter(data);
-      setNotes(data);
+    NoteManager.list().then(loadedNotes => {
+      setNotesRAW(loadedNotes);
+      setNotes(loadedNotes);
     });
   }, []);
-
-  const pureNotes = [
-    { id: 11, text: "première note" },
-    { id: 12, text: "deuxième note" },
-    { id: 33, text: "troisième note" }
-  ]
-
-  const [notesRAW, notesRAWSetter] = useState([...pureNotes]);
-
-  const [notes, setNotes] = useState([...notesRAW]);
-
-
-  const [filters, filtersSetter] = useState(
-    {keyword: ''}
-  );
+  // le tableau vide fait que nous n'attendons pas qu'un setState précis soit
+  // appelé pour déclenché cet effet. Il se déclenchera donc dès que le composant
+  // aura été initialisé.
 
   function onRemoveBtnHandler(noteToDelete) {
+    // mise à jour de l'état
     const noteRawNewValues = ArrayLib.remove(notesRAW, noteToDelete);
-    notesRAWSetter(noteRawNewValues);
-    updateFiltered(noteRawNewValues);
+    setNotesRAW(noteRawNewValues);
+    setNotes(noteRawNewValues);
+
+    // Appel serveur
+    if (noteToDelete.id) {
+      NoteManager
+        .remove(noteToDelete.id)
+        .then(response => console.log('note supprimé côté serveur'))
+        ;
+    }
   }
 
   function onNoteAddedHandler(newNote) {
+    // mise à jour des états
     const noteRawNewValues = [...notesRAW, newNote];
-    notesRAWSetter(noteRawNewValues);
-    updateFiltered(noteRawNewValues)
-  }
+    setNotesRAW(noteRawNewValues);
+    setNotes(noteRawNewValues);
 
-  function updateFiltered(notes) {
-    setNotes([...notes]);
+    // Appel serveur
+    NoteManager.create(newNote)
+      // @workaround Rechargement des notes pour obtenir l'id de la nouvelle note
+      .then(() => NoteManager.list())
+      .then(data => {
+        setNotesRAW(data);
+        setNotes(data);
+      })
+      ;
   }
 
   function onFilterChangedHandler(keyword) {
-    console.log('filters: ', keyword)
-    filtersSetter({
-      keyword: keyword
-    });
-    if (keyword.length > 0) setNotes(notesRAW.filter(n => n.text.includes(keyword)));
-    else setNotes(notesRAW);
+    filtersSetter({ keyword: keyword });
+    if (keyword.length > 0) {
+      setNotes(notesRAW.filter(n => n.text.toLowerCase().includes(keyword.toLowerCase())));
+    } else {
+      setNotes(notesRAW);
+    }
   }
 
   return (
@@ -74,4 +85,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
